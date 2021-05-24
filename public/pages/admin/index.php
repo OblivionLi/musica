b200c415c5f08ac657907067f36782f0e1dd5e5f
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-05-24 16:51:05 +0300

    Add the admin area main page

---
+++ public/pages/admin/index.php
@@ -0,0 +1,10 @@
+<?php require_once('../../../private/initialize.php') ?>
+
+<?php include_once(SHARED_PATH . '/admin/admin-header.php'); ?>
+
+<section class="admin-main">
+    <p>Welcome to the <span>Administration Area </span>!</p>
+    <p>Here you can CREATE/UPDATE/DELETE everything within the DB.</p>
+</section>
+
+<?php include_once(SHARED_PATH . '/admin/admin-footer.php'); ?>
